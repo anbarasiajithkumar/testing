7a989d3a52392a2a081ebd17ae1250db806e4f40
anbarasi <anbarasi@MAC-LAP-113.macrosoftindia.com>
2018-07-31 12:12:16 +0530

    test

--- zephyr/src/zephyr/zephyrc.java
+++ zephyr/src/zephyr/zephyrc.java
@@ -1,10 +1,11 @@
 package zephyr;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.testng.annotations.Test;
-
+import org.junit.*;
+//testing
 public class zephyrc 
 {
 	@Test
@@ -13,9 +14,18 @@ public class zephyrc
 		System.out.println(" Welcome to my world");
 		System.setProperty("webdriver.chrome.driver","F:\\selenium-files\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
-		driver.quit();
-	}
-
+		String aTitle ="";
+driver.get("https://www.google.com");
+driver.manage().window().maximize();
+aTitle= driver.getTitle();
+if (aTitle.contentEquals("Google"))
+{
+	System.out.println("Test Passed");
+}
+else
+{
+	System.out.println("Test failed");	
+}
+driver.quit();
+}
 }
